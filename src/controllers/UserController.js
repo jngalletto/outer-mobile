4bbf48a1c72d1dbbae1c6f574a2b4799e507200f
Joaquin <jngalletto@gmail.com>
2019-07-23 19:25:56 -0300

    add sign in http call

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -23,37 +23,21 @@ class UserController {
     }
   }
 
-  login = async (email, password) =>
-  // Real implementation of a login request using the HttpClient
-  /* try {
-    const result = await httpClient.post({
-      url: `${this.basePath}/session`,
-      method: 'POST',
-      data: {
-        email,
-        password,
-      },
-    });
-    return result.data.user;
-    // Data is the object exposes by axios for the response json
-  } catch (error) {
-    return error;
+  login = async (email, password) => {
+    try {
+      const result = await httpClient.post(`${this.basePath}/sign_in`, {
+        user: {
+          email,
+          password,
+        },
+      });
+      return result.data.user;
+      // Data is the object exposes by axios for the response json
+    } catch (error) {
+      return error;
+    }
   }
-  */
-    // This is a mocked example to simulate api behavior
-    new Promise((resolve, reject) => {
-      if (email !== 'a@a.com' && password !== '') {
-        setTimeout(
-          () => resolve({ name: 'Jorge' }),
-          1000,
-        );
-      } else {
-        setTimeout(
-          () => reject(new Error('Invalid Email/Password')),
-          1000,
-        );
-      }
-    });
+
 
   logout = () => null;
 }
