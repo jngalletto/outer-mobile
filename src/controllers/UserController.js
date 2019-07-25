14fada1eb44a75f3a822545711d6f71e376891e2
Joaquin <jngalletto@gmail.com>
2019-07-25 19:20:23 -0300

    [OUTER-2] - Feature login (#3)

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -16,44 +16,26 @@ class UserController {
           username,
         },
       });
-      return result.data.user;
-      // Data is the object exposes by axios for the response json
+      return Promise.resolve(result.data.user);
     } catch (error) {
-      return error;
+      return Promise.reject(error);
     }
   }
 
-  login = async (email, password) =>
-  // Real implementation of a login request using the HttpClient
-  /* try {
-    const result = await httpClient.post({
-      url: `${this.basePath}/session`,
-      method: 'POST',
-      data: {
-        email,
-        password,
-      },
-    });
-    return result.data.user;
-    // Data is the object exposes by axios for the response json
-  } catch (error) {
-    return error;
+  login = async (email, password) => {
+    try {
+      const result = await httpClient.post(`${this.basePath}/sign_in`, {
+        user: {
+          email,
+          password,
+        },
+      });
+      return Promise.resolve(result.data.user);
+    } catch (error) {
+      return Promise.reject(error);
+    }
   }
-  */
-    // This is a mocked example to simulate api behavior
-    new Promise((resolve, reject) => {
-      if (email !== 'a@a.com' && password !== '') {
-        setTimeout(
-          () => resolve({ name: 'Jorge' }),
-          1000,
-        );
-      } else {
-        setTimeout(
-          () => reject(new Error('Invalid Email/Password')),
-          1000,
-        );
-      }
-    });
+
 
   logout = () => null;
 }
